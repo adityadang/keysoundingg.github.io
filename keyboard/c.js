74fbacf91672b84943068a1bbb6040c10d88d16c
adityadang <103493891+adityadang@users.noreply.github.com>
2022-05-11 17:47:00 +0530

    aditya

---
+++ keyboard/c.js
@@ -0,0 +1,280 @@
+const audio = document.querySelector('audio');
+const audioSource = document.querySelectorAll('.key');
+const aa = document.querySelector('.aa');
+const bb=document.querySelector('.bb');
+const cc=document.querySelector('.cc');
+const dd=document.querySelector('.dd');
+const ee=document.querySelector('.ee');
+const ff=document.querySelector('.ff');
+const gg=document.querySelector('.gg');
+const hh=document.querySelector('.hh');
+const ii=document.querySelector('.ii');
+const jj=document.querySelector('.jj');
+const kk=document.querySelector('.kk');
+const ll=document.querySelector('.ll');
+const mm=document.querySelector('.mm');
+const nn=document.querySelector('.nn');
+const oo=document.querySelector('.oo');
+const pp=document.querySelector('.pp');
+const qq=document.querySelector('.qq');
+const rr=document.querySelector('.rr');
+const ss=document.querySelector('.ss');
+const tt=document.querySelector('.tt');
+const uu=document.querySelector('.uu');
+const vv=document.querySelector('.vv');
+const ww=document.querySelector('.ww');
+const xx=document.querySelector('.xx');
+const yy=document.querySelector('.yy');
+const zz=document.querySelector('.zz');
+
+const a = document.querySelector('.a');
+const b = document.querySelector('.b');
+const c = document.querySelector('.c');
+const d = document.querySelector('.d');
+//class of e is ab
+const ab = document.querySelector('.ab');
+const f = document.querySelector('.f');
+const g = document.querySelector('.g');
+const h = document.querySelector('.h');
+const i = document.querySelector('.i');
+const j = document.querySelector('.j');
+const k = document.querySelector('.k');
+const l = document.querySelector('.l');
+const m = document.querySelector('.m');
+const n = document.querySelector('.n');
+const o = document.querySelector('.o');
+const p = document.querySelector('.p');
+const q = document.querySelector('.q');
+const r = document.querySelector('.r');
+const s = document.querySelector('.s');
+const t = document.querySelector('.t');
+const u = document.querySelector('.u');
+const v = document.querySelector('.v');
+const w = document.querySelector('.w');
+const x = document.querySelector('.x');
+const y = document.querySelector('.y');
+const z = document.querySelector('.z');
+
+//ascii value of a
+
+document.addEventListener('keydown',function(e){
+        if(e.keyCode==65){
+        aa.play();
+        aa.currentTime=0;
+        a.classList.add('animate');
+        setTimeout(function(e){
+            a.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==66){
+        bb.play();
+        bb.currentTime=0;
+        b.classList.add('animate');
+        setTimeout(function(e){
+        b.classList.remove('animate');
+
+        },200);
+
+    }else if(e.keyCode==67){
+        cc.play();
+        cc.currentTime=0;
+        c.classList.add('animate');
+        setTimeout(function(e){
+        c.classList.remove('animate');
+    },200);
+
+
+    }
+    else if(e.keyCode==68){
+        dd.play();
+        dd.currentTime=0;
+        d.classList.add('animate');
+        setTimeout(function(e){
+        d.classList.remove('animate');
+
+    },200);
+    }
+    //e class is taken by me is ab
+    else if(e.keyCode==69){
+        ee.play();
+        ee.currentTime=0;
+        ab.classList.add('animate');
+        setTimeout(function(e){
+            ab.classList.remove('animate');
+        },200);
+
+    }
+    else if(e.keyCode==70){
+        ff.play();
+        ff.currentTime=0;
+        f.classList.add('animate');
+        setTimeout(function(e){
+            f.classList.remove('animate');
+        },200);
+    }else if(e.keyCode==71){
+        gg.play();
+        gg.currentTime=0;
+        g.classList.add('animate');
+        setTimeout(function(e){
+            g.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==72){
+        hh.play();
+        hh.setTimeout=0;
+        h.classList.add('animate');
+        setTimeout(function(e){
+            h.classList.remove('animate');
+
+        },200);
+    }
+    else if(e.keyCode==73){
+        ii.play();
+        ii.currentTime=0;
+        i.classList.add('animate');
+        setTimeout(function(e){
+            i.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==74){
+        jj.play();
+        jj.currentTime=0;
+        j.classList.add('animate');
+        setTimeout(function(e){
+            j.classList.remove('animate');
+        },200);
+
+
+    }else if(e.keyCode==75){
+        kk.play();
+        kk.currentTime=0;
+        k.classList.add('animate');
+        setTimeout(function(e){
+            k.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==76){
+        ll.play();
+        ll.currentTime=0;
+        l.classList.add('animate');
+        setTimeout(function(e){
+            l.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==77){
+        mm.play();
+        mm.currentTime=0;
+        m.classList.add('animate');
+        setTimeout(function(e){
+            m.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==78){
+        nn.play();
+        nn.currentTime=0;
+        n.classList.add('animate');
+        setTimeout(function(e){
+            n.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==79){
+        oo.play();
+        oo.currentTime=0;
+        o.classList.add('animate');
+        setTimeout(function(e){
+            o.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==80){
+        pp.play();
+        pp.currentTime=0;
+        p.classList.add('animate');
+        setTimeout(function(e){
+            p.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==81){
+        qq.play();
+        qq.currentTime=0;
+        q.classList.add('animate');
+        setTimeout(function(e){
+            q.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==82){
+        rr.play();
+        rr.currentTime=0;
+        r.classList.add('animate');
+        setTimeout(function(e){
+            r.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==83){
+        ss.play();
+        ss.currentTime=0;
+        s.classList.add('animate');
+        setTimeout(function(e){
+            s.classList.remove('animate');
+        },200);
+
+    }
+    else if(e.keyCode==84){
+        tt.play();
+        tt.currentTime=0;
+        t.classList.add('animate');
+        setTimeout(function(e){
+            t.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==85){
+        uu.play();
+        uu.currentTime=0;
+        u.classList.add('animate');
+        setTimeout(function(e){
+            u.classList.remove('animate');
+        },200);
+
+    }
+    else if(e.keyCode==86){
+        vv.play();
+        vv.currentTime=0;
+        v.classList.add('animate');
+        setTimeout(function(e){
+            v.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==87){
+        ww.play();
+        ww.currentTime=0;
+        w.classList.add('animate');
+        setTimeout(function(e){
+            w.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==88){
+        xx.play();
+        xx.currentTime=0;
+        x.classList.add('animate');
+        setTimeout(function(e){
+            x.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==89){
+        yy.play();
+        yy.currentTime=0;
+        y.classList.add('animate');
+        setTimeout(function(e){
+            y.classList.remove('animate');
+        },200);
+    }
+    else if(e.keyCode==90){
+        zz.play();
+        zz.currentTime=0;
+        z.classList.add('animate');
+        setTimeout(function(e){
+            z.classList.remove('animate');
+        },200);
+    }
+
+
+});
